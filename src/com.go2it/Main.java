8670e97580b748206e39a720428a6c53cdafbc8c
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-03-14 23:24:35 -0400

    Initial commit

---
+++ src/com.go2it/Main.java
@@ -0,0 +1,27 @@
+package com.go2it;
+
+
+/**
+ * This class was created for learningn purposes.
+ * Students will learn basics of GIT with it
+ *
+ * @author Alex Ryzhkov
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello calculator program");
+		int sum = sum(1,2);
+		int mmultiply = multiply(4,3);
+		System.out.println("Sum is: " + sum + "; multiplication result is: " + mmultiply);
+
+	}
+
+	public static int sum(int i1, int i2){
+		return i1 + i2;
+	}
+
+	public static int multiply(int i1, int i2) {
+		return i1 * i2;
+	}
+}

05f5e9fa076df342d051da0973c000deb36d0f83
olga <odiakonova@mysenca.ca>
2020-02-09 21:55:59 -0500

    Olga added substration and division functions

--- src/com/go2it/Main.java
+++ src/com/go2it/Main.java
@@ -12,8 +12,21 @@ public class Main {
 		return i/j;
 	}
 
+/**@adding two functions by Olga**/
+	public static int substractOlga(int i, int j) {return i-j;}
+
+	public static int divideOlga(int i, int j) {
+		if (j != 0) {
+			i / j
+		} else {
+			System.out.println("Division by Zero!");
+		}
+	}
+/*end of Olga's code*/
 
 	public static void main(String[] args) {
 		sum(1, 2);
+		substractOlga(5, 3);
+		divideOlga(8,4);
 	}
 }

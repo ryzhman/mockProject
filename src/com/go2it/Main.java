3a068a983995c5e6e6ff603a9f845e1bb2191e0a
Bohdan Milenko <bogdan57milenko@gmail.com>
2020-02-10 22:12:40 -0500

    Added new method

--- src/com/go2it/Main.java
+++ src/com/go2it/Main.java
@@ -4,16 +4,26 @@ package com.go2it;
  * @author Alex Ryzhkov
  */
 public class Main {
-	public static int sum(int i, int j) {
-		return i + j;
-	}
+    public static int sum(int i, int j) {
+        return i + j;
+    }
 
-	public static int divideAlex(int i, int j) {
-		return i/j;
-	}
+    public static int divideAlex(int i, int j) {
+        return i / j;
+    }
 
 
-	public static void main(String[] args) {
-		sum(1, 2);
-	}
+    public static void main(String[] args) {
+        sum(1, 2);
+
+		System.out.println(modulusDiv(10,3));
+    }
+
+    public static boolean modulusDiv(int firstInt, int secondInt) {
+        if (firstInt % secondInt == 0) {
+            return true;
+        }
+        return false;
+    }
+
 }

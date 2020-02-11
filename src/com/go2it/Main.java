ab454e117b5bf31ab9e6c7f5e083bff50a87aa2e
olga <odiakonova@mysenca.ca>
2020-02-10 22:15:00 -0500

    Olga's commit with new function

--- src/com/go2it/Main.java
+++ src/com/go2it/Main.java
@@ -13,20 +13,14 @@ public class Main {
 	}
 
 /**@adding two functions by Olga**/
-	public static int substractOlga(int i, int j) {return i-j;}
+public static boolean modulusDivision(int i, int j){
+	return i%j==0;
 
-	public static int divideOlga(int i, int j) {
-		if (j != 0) {
-			i / j
-		} else {
-			System.out.println("Division by Zero!");
-		}
-	}
+}
 /*end of Olga's code*/
 
 	public static void main(String[] args) {
 		sum(1, 2);
-		substractOlga(5, 3);
-		divideOlga(8,4);
+		
 	}
 }

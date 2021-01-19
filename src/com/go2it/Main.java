c38b550eb567aa0359f8c8ef7f8aba82f821f7d2
oleksandr.ryzhkov <oleksandr.ryzhkov@caseware.com>
2021-01-18 19:17:58 -0500

    Clean init commit

--- src/com/go2it/Main.java
+++ src/com/go2it/Main.java
@@ -8,31 +8,7 @@ public class Main {
         return i + j;
     }
 
-    public static int divideAlex(int i, int j) {
-        return i / j;
-    }
-
-/**@adding two functions by Olga**/
-//public static boolean modulusDivision(int i, int j){
-//	return i%j==0;
-// here
-//}
-/*end of Olga's code*/
-
     public static void main(String[] args) {
         sum(1, 2);
-
-		System.out.println(modulusDiv(10,3));
     }
-
-    public static boolean modulusDiv(int firstInt, int secondInt) {
-        if (firstInt % secondInt == 0) {
-            return true;
-        }
-        return false;
-    }
-
-	public static boolean modulusDivision(int i,int j){
-		return i%j==0;
-	}
 }
